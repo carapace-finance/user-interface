179367c83ec557e84d183277a8afd369bc4bda3d
Tai <4382091+taisukemino@users.noreply.github.com>
2022-10-07 19:47:33 +0900

    remove moralis env variables

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    MORALIS_APP_ID: process.env.MORALIS_APP_ID,
-    MORALIS_SERVER_URL: process.env.MORALIS_SERVER_URL,
     POOL_CONTRACT_ADDRESS_DEV: process.env.POOL_CONTRACT_ADDRESS_DEV,
     DAI_TOKEN_ADDRESS_DEV: process.env.DAI_TOKEN_ADDRESS_DEV,
     PREM_TOKEN_ADDRESS_DEV: process.env.PREM_TOKEN_ADDRESS_DEV,
