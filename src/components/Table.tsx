e84a2e54bdc433e3bfbcf1102f8da7b3636b5326
Tai <4382091+taisukemino@users.noreply.github.com>
2022-10-27 20:47:14 +0900

    a table component with tailwind

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,99 +1,41 @@
 import { Container, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import MaterialTable from "material-table";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: theme.spacing(3),
-    "&:nth-child(1) .Component-horizontalScrollContainer-26": {
-      borderRadius: theme.spacing(1)
-    }
-  },
-  card: {
-    padding: theme.spacing(3),
-    margin: theme.spacing(2, 0),
-    maxWidth: theme.spacing(20),
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  item: {
-    padding: theme.spacing(1)
-  },
-  containerItem: {
-    display: "grid",
-    gridTemplateColumns: `1fr`,
-    [theme.breakpoints.up("sm")]: {
-      gridTemplateColumns: `repeat(2, 1fr)`
-    },
-    [theme.breakpoints.up("md")]: {
-      gridTemplateColumns: `repeat(3, 1fr)`
-    }
-  },
-  table: {
-    minWidth: 700
-  },
-  cell: {
-    "&:hover": {
-      color: `${theme.palette.primary} !important`,
-      cursor: "pointer"
-    }
-  },
-  button: {
-    display: "inline",
-    width: "100%"
-  }
-}));
 
 const Table = (props) => {
-  const classes = useStyles();
-
   return (
-    <Container className={classes.root} maxWidth="md">
+    <Container maxWidth="md">
       <Typography gutterBottom variant="h6">
         {props.title}
       </Typography>
-      <MaterialTable
-        title={"My Balance"}
-        style={{
-          borderRadius: "10px"
-        }}
-        columns={[
-          {
-            title: "id",
-            field: "id",
-            cellStyle: { fontWeight: 500, width: "25%" },
-            disableClick: true
-          },
-          {
-            title: "Lending Pool",
-            field: "lending_pool",
-            cellStyle: { width: "25%" }
-          },
-          {
-            title: "Protocols",
-            field: "protocols",
-            cellStyle: { width: "25%" }
-          },
-          {
-            title: "Adjusted Yields",
-            field: "adjusted_yields",
-            cellStyle: { width: "25%" }
-          }
-        ]}
-        data={[
-          {
-            id: "#1",
-            lending_pool: "Almavest Basket #6",
-            protocols: "NA",
-            adjusted_yields: "7 - 10%"
-          }
-        ]}
-        options={{
-          toolbar: false,
-          paging: false
-        }}
-      />
+      <table className="table-auto w-full">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Lending Pool</th>
+            <th>Protocols</th>
+            <th>Adjusted Yields</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>#1</td>
+            <td>Almavest Basket #6</td>
+            <td>Goldfinch</td>
+            <td>7 - 10%</td>
+          </tr>
+          <tr>
+            <td>#2</td>
+            <td>Almavest Basket #6</td>
+            <td>Goldfinch</td>
+            <td>7 - 10%</td>
+          </tr>
+          <tr>
+            <td>#3</td>
+            <td>Almavest Basket #6</td>
+            <td>Goldfinch</td>
+            <td>7 - 10%</td>
+          </tr>
+        </tbody>
+      </table>
     </Container>
   );
 };
