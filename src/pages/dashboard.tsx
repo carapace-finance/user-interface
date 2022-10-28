3323065550bbbd1d8c818ee906a821da633ab0ce
Tai <4382091+taisukemino@users.noreply.github.com>
2022-10-28 18:07:00 +0900

    the dashboard with no Table component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,21 +1,81 @@
 import dynamic from "next/dynamic";
-const Table = dynamic(() => import("@components/Table"), {
-  ssr: false
-});
 const TitleAndDescriptions = dynamic(
   () => import("@components/TitleAndDescriptions"),
   { ssr: false }
 );
 
+const protectionPurchases = [
+  {
+    id: 1,
+    name: "Lending Pool #1",
+    protocol: "Goldfinch",
+    adjustedYields: "7 - 10%"
+  },
+  {
+    id: 2,
+    name: "Lending Pool #1",
+    protocol: "Goldfinch",
+    adjustedYields: "7 - 10%"
+  }
+];
+
+const deposits = [
+  {
+    id: 1,
+    protocol: "Goldfinch",
+    APY: "15 - 20%"
+  },
+  {
+    id: 2,
+    protocol: "Goldfinch",
+    APY: "15 - 20%"
+  }
+];
+
 const Dashboard = () => {
   return (
     <div>
-      <TitleAndDescriptions
-        title="Dashboard"
-        buttonExist={false}
-      />
-      <Table title="Protection Purchases" />
-      <Table title="Deposits" />
+      <TitleAndDescriptions title="Dashboard" buttonExist={false} />
+      <div>Protection Purchases</div>
+      <table className="table-auto w-full">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Lending Pool</th>
+            <th>Protocols</th>
+            <th>Adjusted Yields</th>
+          </tr>
+        </thead>
+        <tbody>
+          {protectionPurchases.map((protectionPurchase) => (
+            <tr>
+              <td>{protectionPurchase.id}</td>
+              <td>{protectionPurchase.name}</td>
+              <td>{protectionPurchase.protocol}</td>
+              <td>{protectionPurchase.adjustedYields}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div>Deposits</div>
+      <table className="table-auto w-full">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Protocol</th>
+            <th>APY</th>
+          </tr>
+        </thead>
+        <tbody>
+          {deposits.map((deposit) => (
+            <tr>
+              <td>{deposit.id}</td>
+              <td>{deposit.protocol}</td>
+              <td>{deposit.APY}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
