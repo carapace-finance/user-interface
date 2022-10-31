f798f86f809cc2f6c585a5eff17dc75cc8dc65fd
Tai <4382091+taisukemino@users.noreply.github.com>
2022-10-31 20:56:33 +0900

    buy protection pop up

--- src/pages/lendingPool/[id].tsx
+++ src/pages/lendingPool/[id].tsx
@@ -1,10 +1,29 @@
 import { useRouter } from "next/router";
+import { useState } from "react";
+import BuyProtectionPopUp from "@components/BuyProtectionPopUp";
 
 const LendingPool = () => {
   const router = useRouter();
   const lendingPoolId = router.query.id;
+  const [tradeOpen, setTradeOpen] = useState(false);
 
-  return <div>LendingPool #{lendingPoolId}</div>;
+  return (
+    <div>
+      <div>LendingPool #{lendingPoolId}</div>
+      <button
+        className="border rounded-md px-4 py-2 m-2 transition duration-500 ease select-none focus:outline-none focus:shadow-outline"
+        onClick={() => {
+          setTradeOpen(true);
+        }}
+      >
+        Preview
+      </button>
+      <BuyProtectionPopUp
+        open={tradeOpen}
+        onClose={() => setTradeOpen(false)}
+      />
+    </div>
+  );
 };
 
 export default LendingPool;
