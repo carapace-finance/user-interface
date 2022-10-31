9a7344f95aa0aaf1dc968fd05a4bd9649d727e9f
Tai <4382091+taisukemino@users.noreply.github.com>
2022-10-31 20:56:43 +0900

    lend with protection pop up

--- src/pages/bond/[id].tsx
+++ src/pages/bond/[id].tsx
@@ -1,10 +1,29 @@
 import { useRouter } from "next/router";
+import { useState } from "react";
+import LendWithProtectionPopUp from "@components/LendWithProtectionPopUp";
 
 const Bond = () => {
   const router = useRouter();
   const bondId = router.query.id;
+  const [tradeOpen, setTradeOpen] = useState(false);
 
-  return <div>Bond #{bondId}</div>;
+  return (
+    <div>
+      <div>Bond #{bondId}</div>
+      <button
+        className="border rounded-md px-4 py-2 m-2 transition duration-500 ease select-none focus:outline-none focus:shadow-outline"
+        onClick={() => {
+          setTradeOpen(true);
+        }}
+      >
+        Preview
+      </button>
+      <LendWithProtectionPopUp
+        open={tradeOpen}
+        onClose={() => setTradeOpen(false)}
+      />
+    </div>
+  );
 };
 
 export default Bond;
