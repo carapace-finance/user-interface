dbce634d9016e74be3452dccdf087a4050e0aac0
Tai <4382091+taisukemino@users.noreply.github.com>
2022-11-02 17:25:48 +0900

    add more columns and data in the buy protection table

--- src/pages/buyProtection.tsx
+++ src/pages/buyProtection.tsx
@@ -1,34 +1,54 @@
 import dynamic from "next/dynamic";
 import Link from "next/link";
+import Image from "next/image";
 const TitleAndDescriptions = dynamic(
   () => import("@components/TitleAndDescriptions"),
   { ssr: false }
 );
+import assets from "../assets";
+
+const goldfinchLogo = assets.goldfinch.src;
 
 const lendingPools = [
   {
     id: 1,
-    name: "Lending Pool #1",
-    protocol: "Goldfinch",
-    adjustedYields: "7 - 10%"
+    name: "Almavest Basket #6",
+    protocol: goldfinchLogo,
+    adjustedYields: "7 - 10%",
+    lendingPoolAPY: "17%",
+    CARATokenRewards: "~3.5%",
+    premium: "4 - 7%",
+    timeLeft: "7 Days 8 Hours 2 Mins"
   },
   {
     id: 2,
-    name: "Lending Pool #1",
-    protocol: "Goldfinch",
-    adjustedYields: "7 - 10%"
+    name: "Almavest Basket #6",
+    protocol: goldfinchLogo,
+    adjustedYields: "7 - 10%",
+    lendingPoolAPY: "17%",
+    CARATokenRewards: "~3.5%",
+    premium: "4 - 7%",
+    timeLeft: "7 Days 8 Hours 2 Mins"
   },
   {
     id: 3,
-    name: "Lending Pool #1",
-    protocol: "Goldfinch",
-    adjustedYields: "7 - 10%"
+    name: "Almavest Basket #6",
+    protocol: goldfinchLogo,
+    adjustedYields: "7 - 10%",
+    lendingPoolAPY: "17%",
+    CARATokenRewards: "~3.5%",
+    premium: "4 - 7%",
+    timeLeft: "7 Days 8 Hours 2 Mins"
   },
   {
     id: 4,
-    name: "Lending Pool #1",
-    protocol: "Goldfinch",
-    adjustedYields: "7 - 10%"
+    name: "Almavest Basket #6",
+    protocol: goldfinchLogo,
+    adjustedYields: "7 - 10%",
+    lendingPoolAPY: "17%",
+    CARATokenRewards: "~3.5%",
+    premium: "4 - 7%",
+    timeLeft: "7 Days 8 Hours 2 Mins"
   }
 ];
 
@@ -46,9 +66,13 @@ const BuyProtection = () => {
         <thead>
           <tr>
             <th>id</th>
-            <th>Lending Pool</th>
-            <th>Protocols</th>
+            <th>Name</th>
+            <th>Protocol </th>
             <th>Adjusted Yields</th>
+            <th>Lending Pool APY</th>
+            <th>CARA Token Rewards</th>
+            <th>Premium</th>
+            <th>Time Left</th>
           </tr>
         </thead>
         <tbody>
@@ -56,8 +80,19 @@ const BuyProtection = () => {
             <tr>
               <td>{lendingPool.id}</td>
               <td>{lendingPool.name}</td>
-              <td>{lendingPool.protocol}</td>
+              <td>
+                <Image
+                  src={lendingPool.protocol}
+                  width={24}
+                  height={24}
+                  alt=""
+                />
+              </td>
               <td>{lendingPool.adjustedYields}</td>
+              <td>{lendingPool.lendingPoolAPY}</td>
+              <td>{lendingPool.CARATokenRewards}</td>
+              <td>{lendingPool.premium}</td>
+              <td>{lendingPool.timeLeft}</td>
               <td>
                 <Link
                   key={lendingPool.id}
