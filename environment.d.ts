823223838999e1510403b9d03d3bd28ee8e594b8
Tai <4382091+taisukemino@users.noreply.github.com>
2022-11-04 18:50:04 +0900

    tenderly env variables

---
+++ environment.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    TENDERLY_ETH_MAINNET_FORK_URL: string;
+    TENDERLY_ACCESS_KEY: string;
+    TENDERLY_PROJECT: string;
+    TENDERLY_USER: string;
+  }
+}
