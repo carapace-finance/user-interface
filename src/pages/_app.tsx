cf97aedd2db2254e73c095b944ee165d8342abfd
Tai <4382091+taisukemino@users.noreply.github.com>
2022-11-15 21:39:29 +0900

    tenderly access key rendering in the server

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,19 @@
 import { Web3ReactProvider } from "@web3-react/core";
-import type { AppProps } from "next/app";
-import getWeb3Library from "../utils/providers";
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@material-tailwind/react";
+import getWeb3Library from "../utils/mainnet/providers";
 import "@style/main.css";
 
 import dynamic from "next/dynamic";
 const Header = dynamic(() => import("@components/Header"), { ssr: false });
 const Footer = dynamic(() => import("@components/Footer"), { ssr: false });
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps, tenderlyAccessKey }) {
   return (
     <ThemeProvider>
       <CssBaseline />
       <Web3ReactProvider getLibrary={getWeb3Library}>
-        <Header />
+        <Header tenderlyAccessKey={tenderlyAccessKey} />
         <Component {...pageProps} />
         <Footer />
       </Web3ReactProvider>
