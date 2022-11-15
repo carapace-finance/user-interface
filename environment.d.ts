124894e42589a81a7874044d2b3c47ad1d7bcaa6
Tai <4382091+taisukemino@users.noreply.github.com>
2022-11-15 21:35:52 +0900

    typescript interface for env variables

--- environment.d.ts
+++ environment.d.ts
@@ -1,8 +1,7 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    TENDERLY_ETH_MAINNET_FORK_URL: string;
     TENDERLY_ACCESS_KEY: string;
-    TENDERLY_PROJECT: string;
-    TENDERLY_USER: string;
+    NEXT_PUBLIC_TENDERLY_PROJECT: string;
+    NEXT_PUBLIC_TENDERLY_USER: string;
   }
 }
