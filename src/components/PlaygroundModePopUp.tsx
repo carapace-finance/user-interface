0b7af05ce3ca192f771fd4dad6eddc637640a32a
Tai <4382091+taisukemino@users.noreply.github.com>
2022-11-30 22:13:19 +0900

    the playgroundmode pop up style

--- src/components/PlaygroundModePopUp.tsx
+++ src/components/PlaygroundModePopUp.tsx
@@ -5,28 +5,32 @@ const PlaygroundModePopUp = (props) => {
 
   return (
     <Dialog
-      fullScreen
-      fullWidth
       maxWidth="lg"
       disableScrollLock
       open={open}
       onClose={onClose}
       PaperProps={{
         style: {
-          borderRadius: "10px"
+          borderRadius: "8px"
         }
       }}
     >
-      <DialogTitle>The application is running in playground mode.</DialogTitle>
       <DialogContent>
-        <div>
-          While in simulation mode, you'll be able to explore some features with
-          a starting balance of 1 ETH. Feel free to explore and play around for
-          as long as you'd like.
-        </div>
+        The application is running in playground mode.
         <IconButton aria-label="close" onClick={onClose} size="small">
           close
         </IconButton>
+      </DialogContent>
+      <DialogContent>
+        <ul>
+          <li>
+            While in simulation mode, you'll be able to explore some features
+            with a starting balance of 1 ETH.
+          </li>
+          <li>
+            Feel free to explore and play around for as long as you'd like.
+          </li>
+        </ul>
       </DialogContent>
     </Dialog>
   );
