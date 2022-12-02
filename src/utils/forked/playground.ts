4e49ad715d00a4a64e029ea30340d9061706bd29
vnadoda <vipul@carapace.finance>
2022-12-02 16:55:25 -0600

    Updated playground to setup initial pool conditions

--- src/utils/forked/playground.ts
+++ src/utils/forked/playground.ts
@@ -5,44 +5,70 @@ import {
   formatUSDC,
   parseUSDC
 } from "@utils/usdc";
-import { deleteFork, fillEther, sendTransaction } from "./tenderly";
+import { getDaysInSeconds } from "@utils/utils";
+import {
+  deleteFork,
+  fillEther,
+  moveForwardTime,
+  sendTransaction
+} from "./tenderly";
 import { Playground } from "./types";
 
 export async function preparePlayground(playground: Playground) {
   const { poolCycleManagerInstance, poolFactoryInstance, poolInstance } =
     playground.deployedContracts;
   console.log(
-    "preparing a playground with contracts: ",
+    "Preparing a playground with contracts: ",
     playground.deployedContracts
   );
 
-  const deployer = await playground.provider.getSigner(0);
+  console.log("********** Pool Phase: OpenToSellers **********");
+  console.log("********** Pool Cycle: 1, Day: 1     **********");
 
+  const deployer = await playground.provider.getSigner(0);
+  console.log("Deployer address: ", await deployer.getAddress());
+
+  const user = playground.provider.getSigner(
+    "0x008c84421da5527f462886cec43d2717b686a7e4"
+  );
+  await fillEther(await user.getAddress(), playground.provider);
+  console.log("User address: ", await user.getAddress());
+
+  // Deposit 1
   await transferApproveAndDeposit(
     playground.provider,
     poolInstance,
-    parseUSDC("5000"),
-    deployer
+    parseUSDC("50000"),
+    user
   );
-  console.log("Starting deposit...");
+
+  // Deposit 2
+  await transferApproveAndDeposit(
+    playground.provider,
+    poolInstance,
+    parseUSDC("51000"),
+    user
+  );
+
   console.log(
     "Pool's total sToken underlying: ",
     formatUSDC(await poolInstance.totalSTokenUnderlying())
   );
+  console.log("Completed min capital requirement.");
 
-  // buy protection
-  const protectionBuyer1 = playground.provider.getSigner(
-    "0x008c84421da5527f462886cec43d2717b686a7e4"
-  );
-  await fillEther(await protectionBuyer1.getAddress(), playground.provider);
+  // move the pool to the next phase
+  await poolInstance.connect(deployer).movePoolPhase();
 
+  console.log("********** Pool Phase: OpenToBuyers **********");
+
+  // buy protection 1
   await transferApproveAndBuyProtection(
     playground.provider,
     poolInstance,
-    protectionBuyer1,
+    user,
     "0xd09a57127bc40d680be7cb061c2a6629fe71abef",
     590,
-    parseUSDC("100000"),
+    parseUSDC("150000"),
     30
   );
 
@@ -51,7 +77,96 @@ export async function preparePlayground(playground: Playground) {
     formatEther(await poolInstance.calculateLeverageRatio())
   );
 
-  console.log("Playground run completed!");
+  // move the pool to the next phase(Open)
+  await poolInstance.connect(deployer).movePoolPhase();
+  console.log(
+    "Current Pool Phase: ",
+    (await poolInstance.getPoolInfo()).currentPhase
+  );
+
+  console.log("********** Pool Phase: Open **********");
+
+  // Withdrawal request 1: 10K sTokens
+  // We are in cycle with index 0, so withdrawal index is 2
+  const withdrawalCycleIndex = 2;
+  await requestWithdrawal(
+    poolInstance,
+    user,
+    parseEther("10000"),
+    withdrawalCycleIndex
+  );
+
+  // Move pool to the next cycle (cycle 2)
+  await movePoolCycle(
+    playground.provider,
+    poolInstance,
+    poolCycleManagerInstance
+  );
+
+  console.log("********** Pool Cycle: 2, Day: 31     **********");
+
+  // Deposit 3
+  await transferApproveAndDeposit(
+    playground.provider,
+    poolInstance,
+    parseUSDC("11000"),
+    user
+  );
+
+  // Move pool to the next cycle (cycle 3)
+  await movePoolCycle(
+    playground.provider,
+    poolInstance,
+    poolCycleManagerInstance
+  );
+
+  console.log("********** Pool Cycle: 3, Day: 62     **********");
+
+  console.log("Playground is ready!");
+}
+
+async function movePoolCycle(provider, poolInstance, poolCycleManagerInstance) {
+  const poolInfo = await poolInstance.getPoolInfo();
+
+  // move from open to locked state
+  await moveForwardTime(provider, getDaysInSeconds(11));
+  await poolCycleManagerInstance.calculateAndSetPoolCycleState(poolInfo.poolId);
+  console.log(
+    "Pool Cycle:",
+    await poolCycleManagerInstance.getCurrentPoolCycle(poolInfo.poolId)
+  );
+
+  // move to new cycle
+  await moveForwardTime(provider, getDaysInSeconds(20));
+  await poolCycleManagerInstance.calculateAndSetPoolCycleState(poolInfo.poolId);
+  console.log(
+    "Pool Cycle:",
+    await poolCycleManagerInstance.getCurrentPoolCycle(poolInfo.poolId)
+  );
+}
+
+async function requestWithdrawal(
+  poolInstance,
+  user,
+  sTokenAmt,
+  withdrawalCycleIndex
+) {
+  await poolInstance.connect(user).requestWithdrawal(sTokenAmt);
+
+  console.log(
+    "User's requested withdrawal Amount: ",
+    formatEther(
+      await poolInstance
+        .connect(user)
+        .getRequestedWithdrawalAmount(withdrawalCycleIndex)
+    )
+  );
+  console.log(
+    "Total requested withdrawal amount: ",
+    formatEther(
+      await poolInstance.getTotalRequestedWithdrawalAmount(withdrawalCycleIndex)
+    )
+  );
 }
 
 export async function transferApproveAndDeposit(
@@ -125,7 +240,7 @@ export async function transferApproveAndBuyProtection(
     lendingPoolAddress: lendingPoolAddress,
     nftLpTokenId: nftLpTokenId,
     protectionAmount: protectionAmount,
-    protectionDurationInSeconds: 86400 * protectionDurationInDays
+    protectionDurationInSeconds: getDaysInSeconds(protectionDurationInDays)
   };
 
   // await poolInstance.connect(buyer).buyProtection(purchaseParams, {
@@ -172,6 +287,9 @@ export async function resetPlayground(playground: Playground) {
   );
 }
 
-export async function deletePlayground(forkId: string, tenderlyAccessKey: string) {
+export async function deletePlayground(
+  forkId: string,
+  tenderlyAccessKey: string
+) {
   await deleteFork(forkId, tenderlyAccessKey);
 }
