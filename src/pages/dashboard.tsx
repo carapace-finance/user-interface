f74fc351acd525611a2bbdfe1f8057a80ba58b33
Tai <4382091+taisukemino@users.noreply.github.com>
2022-12-02 14:08:26 +0900

    implement dashboard structure + some styling

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,21 +1,32 @@
+import { useState } from "react";
 import dynamic from "next/dynamic";
 const TitleAndDescriptions = dynamic(
   () => import("@components/TitleAndDescriptions"),
   { ssr: false }
 );
+import WithdrawalRequestPopUp from "@components/WithdrawalRequestPopUp";
+import WithdrawPopUp from "@components/WithdrawPopUp";
 
 const protectionPurchases = [
   {
     id: 1,
     name: "Lending Pool #1",
     protocol: "Goldfinch",
-    adjustedYields: "7 - 10%"
+    adjustedYields: "7 - 10%",
+    lendingPoolAPY: "17%",
+    CARATokenRewards: "~3.5%",
+    premium: "4 - 7%",
+    timeUntilExpiration: "7 Days 8 Hours 2 Mins"
   },
   {
     id: 2,
     name: "Lending Pool #1",
     protocol: "Goldfinch",
-    adjustedYields: "7 - 10%"
+    adjustedYields: "7 - 10%",
+    lendingPoolAPY: "17%",
+    CARATokenRewards: "~3.5%",
+    premium: "4 - 7%",
+    timeUntilExpiration: "7 Days 8 Hours 2 Mins"
   }
 ];
 
@@ -23,27 +34,43 @@ const deposits = [
   {
     id: 1,
     protocol: "Goldfinch",
-    APY: "15 - 20%"
+    APY: "15 - 20%",
+    totalCapital: "$1,803,000",
+    totalProtection: "$2,203,000",
+    depositedAmount: "$203,000",
+    requestedWithdrawal: "$203,000"
   },
   {
     id: 2,
     protocol: "Goldfinch",
-    APY: "15 - 20%"
+    APY: "15 - 20%",
+    totalCapital: "$1,803,000",
+    totalProtection: "$2,203,000",
+    depositedAmount: "$203,000",
+    requestedWithdrawal: "$203,000"
   }
 ];
 
 const Dashboard = () => {
+  const [isWithdrawalRequestOpen, setIsWithdrawalRequestOpen] = useState(false);
+  const [isWithdrawOpen, setIsWithdrawOpen] = useState(false);
+
   return (
     <div>
       <TitleAndDescriptions title="Dashboard" buttonExist={false} />
-      <div>Protection Purchases</div>
+      <h3 className="text-left">Protection Purchases</h3>
       <table className="table-auto w-full">
         <thead>
           <tr>
             <th>id</th>
             <th>Lending Pool</th>
-            <th>Protocols</th>
+            <th>Protocol</th>
             <th>Adjusted Yields</th>
+            <th>Lending Pool APY</th>
+            <th>CARA Token Rewards</th>
+            <th>Premium</th>
+            <th>Time Until Expiration</th>
+            <th>Claim</th>
           </tr>
         </thead>
         <tbody>
@@ -53,17 +80,30 @@ const Dashboard = () => {
               <td>{protectionPurchase.name}</td>
               <td>{protectionPurchase.protocol}</td>
               <td>{protectionPurchase.adjustedYields}</td>
+              <td>{protectionPurchase.lendingPoolAPY}</td>
+              <td>{protectionPurchase.CARATokenRewards}</td>
+              <td>{protectionPurchase.premium}</td>
+              <td>{protectionPurchase.timeUntilExpiration}</td>
+              <td>
+                <button>claim</button>
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
-      <div>Deposits</div>
+      <h3 className="text-left">Deposits</h3>
       <table className="table-auto w-full">
         <thead>
           <tr>
             <th>id</th>
-            <th>Protocol</th>
+            <th>Protocols</th>
             <th>APY</th>
+            <th>Total Capital</th>
+            <th>Total Protection</th>
+            <th>Deposited Amount</th>
+            <th>Requested Withdrawal</th>
+            <th>Request Withdrawal</th>
+            <th>Withdraw</th>
           </tr>
         </thead>
         <tbody>
@@ -72,10 +112,32 @@ const Dashboard = () => {
               <td>{deposit.id}</td>
               <td>{deposit.protocol}</td>
               <td>{deposit.APY}</td>
+              <td>{deposit.totalCapital}</td>
+              <td>{deposit.totalProtection}</td>
+              <td>{deposit.depositedAmount}</td>
+              <td>{deposit.requestedWithdrawal}</td>
+              <td>
+                <button onClick={() => setIsWithdrawalRequestOpen(true)}>
+                  request withdrawal
+                </button>
+              </td>
+              <td>
+                <button onClick={() => setIsWithdrawOpen(true)}>
+                  withdraw
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
+      <WithdrawalRequestPopUp
+        open={isWithdrawalRequestOpen}
+        onClose={() => setIsWithdrawalRequestOpen(false)}
+      ></WithdrawalRequestPopUp>
+      <WithdrawPopUp
+        open={isWithdrawOpen}
+        onClose={() => setIsWithdrawOpen(false)}
+      ></WithdrawPopUp>
     </div>
   );
 };
