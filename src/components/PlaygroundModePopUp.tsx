52f2a9f2c947fcb3d175917af993aecc237a98f5
Tai <4382091+taisukemino@users.noreply.github.com>
2022-12-16 23:19:47 +0900

    brush up the playground mode pop up texts

--- src/components/PlaygroundModePopUp.tsx
+++ src/components/PlaygroundModePopUp.tsx
@@ -32,14 +32,14 @@ const PlaygroundModePopUp = (props) => {
         <div className="px-8">
           <h2 className="mt-6 mb-8 font-medium text-2xl">Carapace Playground Mode</h2>
           <div className="text-left text-customPopupGrey w-96">
-            <p>Following are the things to take into consideration:</p>
+            <h4 className="mb-2">You can test different features in our playground mode:</h4>
             <ul className="list-disc pl-4">
-              <li>
-                You can test different features like deposit, withdrawal request,
-                withdrawal, and buy protection
-              </li>
-              <li>The set up may take some time</li>
+              <li>buy protection</li>
+              <li>sell protection(deposit)</li>
+              <li>make a request to withdraw</li>
+              <li>withdraw</li>
             </ul>
+            <p className="mt-2">*You have no need to use your wallet and there is no real money.</p>
             {playground?.poolFactoryAddress ? (
             <button
               className="border rounded-full border-gray-400  w-full px-4 py-4 mt-8 mb-2 transition duration-500 ease select-none focus:outline-none focus:shadow-outline"
