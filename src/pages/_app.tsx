9e99dcba89afc9bececaa56e52b6afcb6f7a9b02
Tai <4382091+taisukemino@users.noreply.github.com>
2023-01-24 12:07:58 +0900

    rename the fathom env variable

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -31,7 +31,7 @@ function App({ Component, pageProps }) {
 
   useEffect(() => {
     // Initialize Fathom when the app loads
-    Fathom.load(process.env.FATHOM_SITE_ID, {
+    Fathom.load(process.env.NEXT_PUBLIC_FATHOM_SITE_ID, {
       includedDomains: [
         "dev.carapace.finance",
         "test.carapace.finance",
