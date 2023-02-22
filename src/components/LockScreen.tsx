7ff01c2e4c3b95a17df6d1f35173b5d8e3f78d77
Yuji Yamaguchi <yuji.ym@gmail.com>
2023-02-22 09:52:49 +0900

    fix typing

--- src/components/LockScreen.tsx
+++ src/components/LockScreen.tsx
@@ -5,7 +5,7 @@ import { useNetwork } from "wagmi";
 const LockScreen = () => {
   const [show, setShow] = useState<boolean>(false);
 
-  // set unsupported network
+  // unsupported network
   const { chain } = useNetwork();
   useEffect(() => {
     if (!!chain) {
@@ -15,17 +15,21 @@ const LockScreen = () => {
     }
   }, [chain]);
 
-  return show ? (
-    <div className="fixed top-0 right-0 left-0 bottom-0 bg-white/80 flex items-center justify-center z-40">
-      <div className="bg-customPristineWhite rounded-md shadow-lg z-50 py-12 px-10 text-center">
-        <h1 className="text-customPink flex items-center text-lg mb-4">
-          <AlertTriangle size={20} />
-          &nbsp;Unsupported Network
-        </h1>
-        <p className="text-customGrey">Please connect Mainnet</p>
-      </div>
-    </div>
-  ) : null;
+  return (
+    <>
+      {show ? (
+        <div className="fixed top-0 right-0 left-0 bottom-0 bg-white/80 flex items-center justify-center z-40">
+          <div className="bg-customPristineWhite rounded-md shadow-lg z-50 py-12 px-10 text-center">
+            <h1 className="text-customPink flex items-center text-lg mb-4">
+              <AlertTriangle size={20} />
+              &nbsp;Unsupported Network
+            </h1>
+            <p className="text-customGrey">Please connect Mainnet</p>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
 };
 
 export default LockScreen;
