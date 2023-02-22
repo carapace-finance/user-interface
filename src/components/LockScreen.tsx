5e6f9e81786ab8a4291cb040c88c58a26f6be5f2
Yuji Yamaguchi <yuji.ym@gmail.com>
2023-02-22 10:29:01 +0900

    refactor lockscreen

--- src/components/LockScreen.tsx
+++ src/components/LockScreen.tsx
@@ -3,31 +3,50 @@ import { AlertTriangle } from "lucide-react";
 import { useNetwork } from "wagmi";
 
 const LockScreen = () => {
-  const [show, setShow] = useState<boolean>(false);
+  const [show, setShow] = useState<string | undefined>(undefined);
 
   // unsupported network
   const { chain } = useNetwork();
   useEffect(() => {
     if (!!chain) {
-      chain?.unsupported ? setShow(true) : setShow(false);
+      chain?.unsupported ? setShow("unsuported") : setShow(undefined);
     } else {
-      setShow(false);
+      setShow(undefined);
     }
   }, [chain]);
 
+  const ModalContent = ({
+    title,
+    message
+  }: {
+    title: string;
+    message: string;
+  }) => (
+    <>
+      <h1 className="text-customPink flex items-center text-lg mb-4">
+        <AlertTriangle size={20} />
+        &nbsp;{title}
+      </h1>
+      <p className="text-customGrey">{message}</p>
+    </>
+  );
+
+  const UnsupportedNetwork = () => (
+    <ModalContent
+      title="Unsupported Network"
+      message="Please connect Mainnet."
+    />
+  );
+
   return (
     <>
-      {show ? (
+      {!!show && (
         <div className="fixed top-0 right-0 left-0 bottom-0 bg-white/80 flex items-center justify-center z-40">
           <div className="bg-customPristineWhite rounded-md shadow-lg z-50 py-12 px-10 text-center">
-            <h1 className="text-customPink flex items-center text-lg mb-4">
-              <AlertTriangle size={20} />
-              &nbsp;Unsupported Network
-            </h1>
-            <p className="text-customGrey">Please connect Mainnet</p>
+            {show === "unsupported" && <UnsupportedNetwork />}
           </div>
         </div>
-      ) : null}
+      )}
     </>
   );
 };
