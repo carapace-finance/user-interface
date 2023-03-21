c2ddaaa3d0c818f0b958bda752bcc65ec09e84f9
Yuji Yamaguchi <yuji.ym@gmail.com>
2023-03-21 19:03:47 +0900

    remove unused

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,6 @@ const Header = () => {
   const { disconnect } = useDisconnect();
   const router = useRouter();
   const [modalOpen, setModalOpen] = useAtom(connectModalAtom);
-  const [menuOpen, setMenuOpen] = useState<boolean>(false);
   const txs = useAtomValue(userTransactionsAtom);
   const isInternalPage = useMemo(
     () => !["/", "/buy-protection", "/portfolio"].includes(router.pathname),
@@ -27,7 +26,7 @@ const Header = () => {
   );
 
   return (
-    <nav className="header bg-white h-26 md:h-16 md:px-2 sm:px-4 fixed w-full z-30 top-0 left-0 md:shadow-md">
+    <nav className="header bg-white h-26 md:h-16 md:px-2 sm:px-4 fixed w-full z-30 top-0 left-0 md:border-b">
       <div
         className={`container flex flex-wrap items-center justify-between mx-auto h-16 px-3 md:px-0 ${
           isInternalPage ? "border-b" : ""
@@ -77,33 +76,6 @@ const Header = () => {
             open={modalOpen}
             onClose={() => setModalOpen(false)}
           />
-        </div>
-        <div
-          className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
-            menuOpen ? "visible" : "hidden"
-          }`}
-          id="navbar-sticky"
-        >
-          <ul className="fixed right-0 left-0 top-16 text-center md:relative md:top-auto md:right-auto md:left-auto flex flex-col p-4 rounded-lg bg-customPristineWhite md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white shadow-lg md:shadow-none">
-            {HEADER_LINKS.map((item: any) => (
-              <li key={item.key}>
-                <Link
-                  href={item.link}
-                  className={`${
-                    item.activePaths.includes(
-                      router.pathname.split("/").length > 2
-                        ? `/${router.pathname.split("/")[1]}/`
-                        : router.pathname
-                    )
-                      ? "text-customBlue font-medium"
-                      : ""
-                  } hover:text-customBlue block py-2 pl-3 pr-4 rounded md:bg-transparent md:p-0`}
-                >
-                  <h4>{item.title}</h4>
-                </Link>
-              </li>
-            ))}
-          </ul>
         </div>
       </div>
       <div
